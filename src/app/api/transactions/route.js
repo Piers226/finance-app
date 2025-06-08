afd99c3d54abbce0994f84e3f8c56e7d72399284
Piers226 <par226@lehigh.edu>
2025-06-08 14:58:28 -0400

    fixingn transaction access

--- src/app/api/transactions/route.js
+++ src/app/api/transactions/route.js
@@ -6,7 +6,13 @@ import User from '@/models/User';
 
 export async function GET(req) {
   await connectToDatabase();
-  const transactions = await Transaction.find().sort({ date: -1 }).limit(50);
+  const url = new URL(req.url);
+  const userId = url.searchParams.get('userId');
+  
+    if (!userId) {
+      return NextResponse.json({ error: 'Missing userId' }, { status: 400 });
+    }
+  const transactions = await Transaction.find({ userId }).sort({ date: -1 }).limit(50);
   return NextResponse.json(transactions);
 }
 
